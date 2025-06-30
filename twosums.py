01b7457421824d9783f172c68def29c03620828c
Hasnain <hasnainbharmal4@gmail.com>
2025-06-30 18:57:09 -0500

    2nd start to leetcode

--- twosums.py
+++ twosums.py
@@ -9,3 +9,10 @@ class Solution:
 #Finallly my first leetcode OMG ive such a long way to go, by well i got it working and i am happy with it. Did use chatgpt to help in last test since i wasnt able to figure
 #out how to solve the duplicate issue. But i am happy with the result.
 #Learningfrom this is that i need to learn more about python and its libraries and also need to learn more about the data structures and algorithms. And list.index uses () not []
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+           for m in range(len(nums)):
+            if (nums[i] + nums[m] == target) and i !=m :
+                return [i,m] 
